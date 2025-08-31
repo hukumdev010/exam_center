c2d27b7d3b6af81766d9c8c8a9bd962fb4697558
hukum basnet <basnethukum789@gmail.com>
2025-08-31 16:33:48 +0000

    Fix on auth utils

--- src/lib/auth-utils.ts
+++ src/lib/auth-utils.ts
@@ -1,7 +1,6 @@
 import { getServerSession } from "next-auth"
-import { getAuthOptions } from "@/lib/auth"
+import { authOptions } from "@/lib/auth"
 
 export async function getAuthSession() {
-  const authOptions = await getAuthOptions()
   return await getServerSession(authOptions)
 }
